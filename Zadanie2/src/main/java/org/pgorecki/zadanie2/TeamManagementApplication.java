df5f8856f04286def6c580a5a1b71f030ebbbc1c
Patryk <patryk.gorecky@gmail.com>
2024-09-25 18:39:52 +0200

    INIT_COMMIT

---
+++ Zadanie2/src/main/java/org/pgorecki/zadanie2/TeamManagementApplication.java
@@ -0,0 +1,13 @@
+package org.pgorecki.zadanie2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamManagementApplication.class, args);
+    }
+
+}
---
+++ Zadanie2/src/test/java/org/pgorecki/zadanie2/TeamManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pgorecki.zadanie2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
