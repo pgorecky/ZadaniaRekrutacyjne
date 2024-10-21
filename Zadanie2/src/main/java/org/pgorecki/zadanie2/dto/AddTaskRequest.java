75ca37eafd9eb00be3bb6a2206f27454dc4362b2
Patryk <patryk.gorecky@gmail.com>
2024-10-21 17:15:41 +0200

    Z2-fix:task request validation

--- Zadanie2/src/main/java/org/pgorecki/zadanie2/dto/AddTaskRequest.java
+++ Zadanie2/src/main/java/org/pgorecki/zadanie2/dto/AddTaskRequest.java
@@ -1,13 +1,21 @@
 package org.pgorecki.zadanie2.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
 import org.pgorecki.zadanie2.model.TaskStatus;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record AddTaskRequest(
+
+        @NotBlank(message = "Title should not be blank")
         String title,
+
+        @NotBlank(message = "Description should not be blank")
         String description,
+        @Enumerated(EnumType.STRING)
         TaskStatus status,
         LocalDate deadline,
         List<Long> assignedUsers) {
